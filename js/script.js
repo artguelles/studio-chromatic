1b66f33f993796c98a97f65ba3c69f5327b5b0fb
artguelles <arguellesmv@gmail.com>
2024-11-05 16:14:48 +0100

    fixed js carousel + added js file + initialization
    falta adaptar el código para que aparezca. lo hago más tarde

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+console.log("Hello world");
+console.log("BY MARIVÍ & CLARA");
+
+
+/*ANIMACIONES AOS*/
+AOS.init();
+
+/*TYPED.JS*/
+var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.','Use your own!'],
+    typeSpeed: 50,
+    loop: true,
+});
+
+/*AL HACER SCROLL, APARECE LOGO SUPERIOR DERECHA*/
+
+
+/*AL LLEGAR AL FOOTER, NAV DESAPARECE JUNTO AL LOGO*/
+
+
+/*VENTANA MODAL FORMULARIO*/
+
